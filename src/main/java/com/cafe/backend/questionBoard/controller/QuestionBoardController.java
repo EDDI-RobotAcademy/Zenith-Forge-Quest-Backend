44de495d66e7048d3b27cb643ffaa675b9435698
jeongdawun <jeongdawun33@gmail.com>
2023-11-03 11:21:05 +0900

    feat: [정다운] API 문서 자동화를 위한 Swagger 구성 [ZFQB-1]

--- src/main/java/com/cafe/backend/questionBoard/controller/QuestionBoardController.java
+++ src/main/java/com/cafe/backend/questionBoard/controller/QuestionBoardController.java
@@ -1,5 +1,8 @@
 package com.cafe.backend.questionBoard.controller;
 
+import com.cafe.backend.questionBoard.controller.form.QuestionTestForm;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -8,9 +11,11 @@ import org.springframework.web.bind.annotation.RestController;
 @Slf4j
 @RestController
 @RequestMapping("/question-board")
+@Tag(name = "Question Board", description = "Question Board API")
 public class QuestionBoardController {
 
     @GetMapping("/test")
+    @Operation(summary = "Question Board Test", description = "질문 게시판 테스트입니다.")
     public String justTest () {
         return "Question Board Test";
     }
